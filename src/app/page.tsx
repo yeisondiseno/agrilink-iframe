b879aac54d28f7d4714267588060b40d49f9bfa1
Yeison Montoya <yeisondiseno@gmail.com>
2024-08-11 12:35:09 -0500

    feat: responsive and init get data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
-import Image from 'next/image';
 // Components
-import { Card } from '@components/index';
+import { Card, Skeleton } from '@components/index';
 import { CardButton } from '@modules/index';
+// Config
+import { env } from '@config/env';
 // Styles
 import './page.scss';
 
-export default function Home() {
+async function getData() {
+  console.log('env', env);
+  const res = await fetch(`${env.urlApi}/sponsors?include=category`);
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+
+  console.log('data', data?.data);
+
   return (
     <main className='home max-block'>
       <section>
         <h1>Patrocinadores</h1>
+
         <Card>
+          <CardButton />
+
+          <CardButton />
+
+          <CardButton />
+
           <CardButton />
         </Card>
       </section>
--- src/components/atoms/index.tsx
+++ src/components/atoms/index.tsx
@@ -4,3 +4,4 @@ export { default as Divider } from './Divider/Divider';
 export { default as Card } from './Card/Card';
 export { default as Portal } from './Portal/Portal';
 export { default as Cover } from './Cover/Cover';
+export { default as Skeleton } from './Skeleton/Skeleton';
